91e1248e1da5ab1be5c8586b77b48b45f5fbcefa
asmat-ahmadi <habib@habibs-MacBook-Air.local>
2023-08-04 11:50:27 -0400

    1th commit

---
+++ Myhomenotes and practice/src/JavaCasting.java
@@ -0,0 +1,6 @@
+public class JavaCasting {
+    public static void main(String[] args) {
+        // casting is the way to convert datatype which is common in Java language //
+       foat =
+    }
+}
