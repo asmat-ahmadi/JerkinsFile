f6e9c3544130a4176e1c49a27324f9447cc33f4d
asmat-ahmadi <habib@habibs-MacBook-Air.local>
2023-09-27 12:30:11 -0400

    f

--- SeleniumBasicB17/src/test/java/FacebookExample.java
+++ SeleniumBasicB17/src/test/java/FacebookExample.java
@@ -1,19 +1,38 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FacebookExample {
     public static void main(String[] args) {
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com");
         driver.manage().window().maximize();
-       WebElement email= driver.findElement(By.id("email"));
-        email.sendKeys("usthabibi@gamil.com");
-        WebElement password=driver.findElement(By.id("pass"));
-        password.sendKeys("adf34456");
-        WebElement  LoginBtn=driver.findElement(By.id("Login"));
-        LoginBtn.click();
 
+        // Find the email/phone input field and enter your email/phone
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("usthabibi@gmail.com");
+
+        // Find the password input field and enter your password
+        WebElement password = driver.findElement(By.id("pass"));
+        password.sendKeys("habib1978");
+
+
+        //WebElement loginBtn = driver.findElement(By.id("u_0_b"));
+       // JavascriptExecutor executor = (JavascriptExecutor) driver;
+       // executor.executeScript("arguments[0].click();", loginBtn);
+        // Find the Login button and click it
+       // WebElement loginBtn = driver.findElement(By.id("login"));
+       // loginBtn.click();
+
+        // You may need to add additional logic here to handle login success or failure
+
+        // Close the browser when done
+        driver.quit();
     }
 }
+
+
--- SeleniumBasicB17/src/test/java/class02/Homework01.java
+++ SeleniumBasicB17/src/test/java/class02/Homework01.java
@@ -9,20 +9,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Homework01 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver =new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.get("https://www.facebook.com");
         driver.manage().window().maximize();
         Thread.sleep(2000);
-        WebElement userName=driver.findElement(By.name("username"));
-        userName.sendKeys("abc");
-        userName.sendKeys(Keys.CONTROL + "a");
-        userName.sendKeys(Keys.DELETE);
-        userName.sendKeys("Admin");
-        WebElement password=driver.findElement(By.name("password"));
-        password.sendKeys("admin123");
-        WebElement loginB=driver.findElement(By.tagName("button"));
-        String log=loginB.getText();
-        System.out.println(log);
+        WebElement email=driver.findElement(By.id("email"));
+       // userName.sendKeys("abc");
+       // userName.clear();
+      //  userName.sendKeys(Keys.CONTROL + "a");
+       // userName.sendKeys(Keys.DELETE);
+        email.sendKeys("usthabibi@gmail.com");
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("habib1978");
+        WebElement loginB=driver.findElement(By.id("login"));
         loginB.click();
+        String log=loginB.getText();
+        System.out.println("mylogin"+log);
+
 
     }
 }
