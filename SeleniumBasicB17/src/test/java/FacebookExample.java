76926a4f352a01d70e0866f96c37defdf94f87e2
asmat-ahmadi <habib@habibs-MacBook-Air.local>
2023-10-04 10:55:49 -0400

    firstcomit

--- SeleniumBasicB17/src/test/java/FacebookExample.java
+++ SeleniumBasicB17/src/test/java/FacebookExample.java
@@ -7,31 +7,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FacebookExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com");
         driver.manage().window().maximize();
+        Thread.sleep(2000);
+        WebElement user=driver.findElement(By.id("email"));
+        user.sendKeys("usthabibi@gmail.com");
+        WebElement pass= driver.findElement(By.id("pass"));
+        pass.sendKeys("ha99hyy76");
+       // WebElement log= driver.findElement(By.name("login"));
+       // log.click();
+        WebElement create=driver .findElement(By.xpath("Create new account"));
+        create.click();
 
-        // Find the email/phone input field and enter your email/phone
-        WebElement email = driver.findElement(By.id("email"));
-        email.sendKeys("usthabibi@gmail.com");
-
-        // Find the password input field and enter your password
-        WebElement password = driver.findElement(By.id("pass"));
-        password.sendKeys("habib1978");
 
 
-        //WebElement loginBtn = driver.findElement(By.id("u_0_b"));
-       // JavascriptExecutor executor = (JavascriptExecutor) driver;
-       // executor.executeScript("arguments[0].click();", loginBtn);
-        // Find the Login button and click it
-       // WebElement loginBtn = driver.findElement(By.id("login"));
-       // loginBtn.click();
 
-        // You may need to add additional logic here to handle login success or failure
-
-        // Close the browser when done
-        driver.quit();
     }
 }
 
